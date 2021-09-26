97a73d8cd80ea991dac7e38f8038e40bbbd102aa
Hugo Ku <hugoku@gmail.com>
2021-09-26 01:37:46 -0400

    implemented a man page for bot commands

--- helperBot.py
+++ helperBot.py
@@ -1,4 +1,5 @@
 import discord
+import hashlib
 
 # import the token
 with open('token') as tokenFile:
@@ -6,6 +7,22 @@ with open('token') as tokenFile:
 
 # boiler plate code
 client = discord.Client()
+
+# parse message contents
+def commands(message):
+	content = message.split()	
+	
+	if content[1] == 'hash':
+		if len(content) != 3:
+			return 'not enough or too many args'
+		hashVal = hashlib.sha256(content[2].encode()).hexdigest()
+		return 'Hashed value is: ' + str(hashVal)
+	
+	if content[1] == 'help':
+		with open('manPage') as helpText:
+			return ''.join(helpText.readlines())
+	
+	return 'received' + ' ' + content[1]
 
 @client.event
 async def on_ready():
@@ -15,8 +32,11 @@ async def on_ready():
 async def on_message(message):
 	if message.author == client.user:
 		return
-
-	if message.content.startswith('$hello'):
-		await message.channel.send('Hello!')
+	
+	if message.content.startswith('h!'):
+		response = commands(message.content)
+		await message.channel.send(response)
 
 client.run(token)
+
+
